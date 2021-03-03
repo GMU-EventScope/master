637aa635c71e90d6b4809ff721a5c62a38f9dcf6
Richard Yao <yao.richard5@gmail.com>
2021-03-03 13:00:21 -0500

    added loading delay for buttons, prob is not best method

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,17 +5,17 @@ import {useRef,useState,useEffect} from "react";
 const useButtonLoader = (defaultText = "Load",loadingText = "Loading...") => {
     const[isLoading,setLoading] = useState(false);
     const element = useRef(null);
+    //Here is the code for switching a button to loading and normal state
     useEffect(() => {
         if(isLoading){
             element.current.disabled=true;
             element.current.innerHTML = 
-            '<i class="fas fa-spinner fa-spin"></i>' + loadingText;
+                '<i class="fas fa-spinner fa-spin"></i>' + loadingText;
         }
         else{
             element.current.disabled=false;
             element.current.innerHTML = defaultText;
         }
-
     }, [isLoading]);
     return [element,setLoading];
 };
--- src/components/EventMarker.js
+++ src/components/EventMarker.js
@@ -2,57 +2,75 @@ import React from 'react'
 import {Button, Card} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './EventMarker.css';
-import useButtonLoader from './Buttons';
+import useButtonLoader from './Buttons.js';
 
-//TODOs: connect to firebase (card image,and just in general for buttons)
+//TODOs: connect to firebase (card image,and just in general for buttons), add artificial delay
 
-//adds artificial delay for loading (cuz too fast)
+//adds artificial delay for loading (cuz too fast, user can't see loading msg)
 function loadingDelay() {
-  return new Promise((resolve) => setTimeout(resolve, 1000));
+  return new Promise((resolve) => setTimeout(resolve, 500));
 }
 
 //buttons help: https://react-bootstrap.github.io/components/buttons/
 //Followed this for loading buttons: https://www.youtube.com/watch?v=nCEnqQABC5A via hooks
 const Marker = (props) => {
   //button hook thing, name the buttons here (static text, loading text)
-  //need to create a new set of consts for each button
+  //useButtonLoader is in Button.js
   const [Button1Load,setButton1] = useButtonLoader("I'm here","Updating..");
   const [Button2Load,setButton2] = useButtonLoader("Save Event","Saving..");
   const [Button3Load,setButton3] = useButtonLoader("Report","Getting Report Ready..");
 
   //eventually change placeholder stuff to connect to firebase
 
+  // might be a bad way of adding 0.5s delay (below in button consts)...
+  // could fetch then do loadingDelay()?
+
   //I'm here
   const Button1 = () => {
+    //essential
     setButton1(true);
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    loadingDelay().then(() => {
+      //fetch stuff is placeholder
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        //delay no work
-        loadingDelay();
+        //esential
         setButton1(false);
       });
+    });
   };
+
   //Save Event
   const Button2 = () => {
+    //essential
     setButton2(true);
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    loadingDelay().then(() => {
+      //fetch stuff is placeholder
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
+        //esential
         setButton2(false);
       });
+    });
   };
+
   //Report
   const Button3 = () => {
+    //essential
     setButton3(true);
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    loadingDelay().then(() => {
+      //fetch stuff is placeholder
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
+        //esential
         setButton3(false);
       });
+    });
   };
 
   // eventually want to make the Card.Img the user uploaded one from db
@@ -80,52 +98,5 @@ const Marker = (props) => {
       </div>
     )
 }
-
-
-//BELOW HAS PROBLEM WHERE ALL BUTTONS WILL GET ACTIVATED
-
-/*
-function loadingDelay() {
-  return new Promise((resolve) => setTimeout(resolve, 1000));
-}
-
-const Marker = (props) => {
-  //Here is the code for switching a button to loading and normal state
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    if (isLoading) {
-      loadingDelay().then(() => {
-        setLoading(false);
-      });
-    }
-  }, [isLoading]);
-
-  const handleClick = () => {setLoading(true); talkToFirebase();};
-
-  
-  // eventually want to make the Card.Img the user submitted one from db
-  // Maybe for the "I'm here" button, disable button and turn red after clicked (and loading)?
-  return (
-      <div>
-        <Card style={{ width: '22rem'}} bg={"success"} border={"warning"}>    
-        <Card.Img variant="top" src="/gmulogo.png" />
-        <Card.Body>
-          <Card.Title>{props.title} - {props.author}</Card.Title>
-          <Card.Text>
-            <h3>{props.date}</h3>
-            {props.context}
-          </Card.Text>
-          <Button variant="primary mr-2" size='lg' block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Updating..." : "I'm here!"}</Button>
-          <Button variant="info mr-2" block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Setting Reminder..." : 'Remind Me'}</Button>
-          <Button variant="danger mr-2" block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Getting Report Ready..." : 'Report'}</Button>
-        </Card.Body>
-      </Card>
-      </div>
-    )
-}
-*/
 
 export default Marker
