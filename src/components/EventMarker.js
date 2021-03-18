44c239006fa783b49fa7b1f8f888d5191b090f5f
Richard Yao <yao.richard5@gmail.com>
2021-03-18 17:31:48 -0400

    updated buttons,mapstyle

--- src/components/EventMarker.js
+++ src/components/EventMarker.js
@@ -1,31 +1,115 @@
-import React,{ useEffect, useState } from 'react'
-
+import React from 'react'
 import {Button, Card} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import './EventMarker.css';
+import useButtonLoader from './Buttons.js';
+import fbArray from '../apis/firebase.js';
 
+// get firebase stuff
+const db = fbArray.db;
+const auth = fbArray.auth;
+
+//TODOs: connect to firebase (card image,and just in general for buttons)
+
+//adds artificial delay for loading (cuz too fast, user can't see loading msg)
+function loadingDelay(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
 
 //buttons help: https://react-bootstrap.github.io/components/buttons/
-
-//This function just simulates a delay of 2s?, eventually we want to send/retrieve from db here
-function talkToFirebase() {
-  return new Promise((resolve) => setTimeout(resolve, 2000));
-}
+//Followed this for loading buttons: https://www.youtube.com/watch?v=nCEnqQABC5A via hooks
 const Marker = (props) => {
-  //Here is the code for switching a button to loading and normal state
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    if (isLoading) {
-      talkToFirebase().then(() => {
-        setLoading(false);
-      });
-    }
-  }, [isLoading]);
+  //button hook thing, name the buttons here (static text, loading text)
+  //useButtonLoader is in Button.js (for loading buttons)
+  const [Button1Load,setLoadingButton1] = useButtonLoader("I'm here","Updating..");
+  const [Button2Load,setLoadingButton2] = useButtonLoader("Save Event","Saving..");
+  const [Button3Load,setLoadingButton3] = useButtonLoader("Report","Getting Report Ready..");
 
-  const handleClick = () => setLoading(true);
+  //eventually change placeholder stuff to connect to firebase
+  //would also need to modify and add user stuff (so ex. save event saves to right account)
+
+  // might be a bad way of adding 0.420s delay (below in button consts)...
+  // could fetch then do loadingDelay()?
+
+  //I'm here
+  const Button1 = () => {
+    //essential
+    setLoadingButton1(true);
+    loadingDelay(220).then(() => {
+      //fetch stuff is placeholder
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        //esential
+        setLoadingButton1(false);
+      });
+    }); 
+  };
+
+  //Save Event
+  const Button2 = () => {
+    //essential
+    setLoadingButton2(true);
+    loadingDelay(420).then(() => {
+      // find current user doc in firestore
+      const user = auth.currentUser;
+      // get current event reference
+      const curEvent = "placeholder value";
+      db.collection("users").doc(user.uid).get().then(doc => {
+          // check if doc exists
+          if (!doc.exists) {
+            // create new document
+            db.collection("users").doc(user.uid).set( {
+              savedevents: curEvent
+            })
+          }
+          else {
+            // get current list of events
+            var myEvents = doc.data().savedevents;
+            
+            // add to list
+            myEvents.push(curEvent);
+            // replace old list on db with new list
+            db.collection("users").doc(user.uid).update({
+              savedevents: myEvents
+            });
+            console.log("done saving event");
+        }
+      });
+      
+      //esential
+      setLoadingButton2(false);
+    });
+  };
+
+  //Report
+  const Button3 = () => {
+    //essential
+    setLoadingButton3(true);
+    loadingDelay(420).then(() => {
+      //fetch stuff is placeholder
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        //esential
+        setLoadingButton3(false);
+      });
+    });
+  };
+
+  // eventually want to make the Card.Img the user uploaded one from db
+  // Maybe for the "I'm here" button, disable the button and turn red after clicked (and loading)?
+  // not sure how we are going to do it per user
   
+  //Below is actual card
+  //Title is like "Godzilla", author is "eminem"
+  //Props.date not used yet?
+  //props.context is description
   return (
       <div>
-        <Card style={{ width: '25rem' }}>
+        <Card style={{ width: '22rem'}} bg={"success"} border={"warning"}>    
         <Card.Img variant="top" src="/gmulogo.png" />
         <Card.Body>
           <Card.Title>{props.title} - {props.author}</Card.Title>
@@ -33,13 +117,12 @@ const Marker = (props) => {
             <h3>{props.date}</h3>
             {props.context}
           </Card.Text>
-          <Button variant="success mr-2" size='lg' block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Mining some bitcoins..." : "I'm here!"}</Button>
-          <Button variant="info mr-2" block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Mining some bitcoins..." : 'Remind Me'}</Button>
-            &nbsp;
-          <Button variant="outline-danger mr-2" size='sm' block disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-            {isLoading ? "Mining some bitcoins..." : 'Report'}</Button>
+          <Button variant="primary mr-2" size='lg' block onClick={Button1} ref={Button1Load}>
+           </Button>
+          <Button variant="info mr-2" size='lg' block onClick={Button2} ref={Button2Load}>
+           </Button>
+          <Button variant="danger mr-2" block onClick={Button3} ref={Button3Load}>
+           </Button>
         </Card.Body>
       </Card>
       </div>
